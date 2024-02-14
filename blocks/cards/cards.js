b01cfd7eddb2d6639b5a8b18dfde683b4ee0ac7f
Vishal Jaiswal <vishal@iamhelium.com.au>
2024-02-14 16:45:37 +1100

    fix: lint error fixes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,36 +1,36 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement("ul");
+  const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector("picture"))
-        div.className = "cards-card-image";
-      else div.className = "cards-card-body";
+      if (div.children.length === 1 && div.querySelector('picture'))
+        div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
-  ul.querySelectorAll("img").forEach((img) =>
+  ul.querySelectorAll('img').forEach((img) =>
     img
-      .closest("picture")
+      .closest('picture')
       .replaceWith(
-        createOptimizedPicture(img.src, img.alt, false, [{ width: "750" }])
+        createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])
       )
   );
-  block.textContent = "";
+  block.textContent = '';
   block.append(ul);
 
   const isSliderBlock =
-    document.getElementsByClassName("cards slider block")[0];
+    document.getElementsByClassName('cards slider block')[0];
   if (isSliderBlock) {
     const cardWrapperElm = isSliderBlock.parentElement;
-    cardWrapperElm.classList.add("cards-slider-wrapper");
+    cardWrapperElm.classList.add('cards-slider-wrapper');
 
     if (cardWrapperElm.parentElement) {
-      cardWrapperElm.parentElement.classList.add( "slider-container");
+      cardWrapperElm.parentElement.classList.add( 'slider-container');
     }
   }
 }
--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,34 +1,34 @@
-import { getMetadata } from "../../scripts/aem.js";
-import { loadFragment } from "../fragment/fragment.js";
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const footerMeta = getMetadata("footer");
-  block.textContent = "";
+  const footerMeta = getMetadata('footer');
+  block.textContent = '';
 
   // load footer fragment
-  const footerPath = footerMeta.footer || "/footer";
+  const footerPath = footerMeta.footer || '/footer';
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
-  const footer = document.createElement("div");
+  const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
 
-  const footerNav = document.getElementsByTagName("columns footer");
+  const footerNav = document.getElementsByTagName('columns footer');
   const footerNavElements = Array.from(
-    document.getElementsByClassName("columns footer")[0].children[0].children
+    document.getElementsByClassName('columns footer')[0].children[0].children
   );
   footerNavElements.map((parentNavEls) => {
     const childNavEls = Array.from(parentNavEls.children);
     childNavEls.map((childElement) => {
-      const hasH3Tag = childElement.tagName.toLowerCase() === "h3";
+      const hasH3Tag = childElement.tagName.toLowerCase() === 'h3';
       if (hasH3Tag) {
-        childElement.classList.add("head-title-link"); // Replace 'yourClassName' with the desired class name
+        childElement.classList.add('head-title-link'); // Replace 'yourClassName' with the desired class name
       }
     });
   });
